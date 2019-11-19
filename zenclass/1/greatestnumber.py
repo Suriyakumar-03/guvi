b7d16d04ab0895f892742a793a96da5f1f1fb45e
Suriyakumar-03 <57931873+Suriyakumar-03@users.noreply.github.com>
2019-11-19 22:16:33 +0530

    Update greatestnumber.py

--- zenclass/1/greatestnumber.py
+++ zenclass/1/greatestnumber.py
@@ -3,12 +3,11 @@ a = int(input("the number is:"))
 b = int(input("the number is:"))
 c = int(input("the number is:"))
 if(a>b):
-	if(a>c):
-		print(" a is the greatest number")
+ if(a>c):
+  print(" a is the greatest number")
 else:
-         print(" c is the greatest number")
 if(b>a):
-	if(b>c):
-		print("b is the greatest number")  
-else:
-        print(" c is the gretest number")
+ if(b>c):
+  print("b is the greatest number")  
+ else:
+  print(" c is the gretest number")
