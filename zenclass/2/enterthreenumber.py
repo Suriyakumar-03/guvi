0076d0349baa9a55998fe874c3570aaf8e2d7ef7
Suriyakumar-03 <57931873+Suriyakumar-03@users.noreply.github.com>
2019-12-12 14:37:02 +0530

    Add files via upload

---
+++ zenclass/2/enterthreenumber.py
@@ -0,0 +1,4 @@
+inputs=[input() for i in range(3)]
+print(inputs)
+for x in inputs:
+ print(x)
---
+++ zenclass/2/list.py
@@ -0,0 +1,57 @@
+#by using LIST methods
+
+#by using APPEND methods
+p=["guvi",2,3]
+p.append("zoho")
+print(p)
+
+#by using CLEAR methods
+a=["guvi",2,3]
+a.clear()
+print(a)
+
+#by using COPY methods
+b=["guvi",2,4]
+b.copy()
+print(b)
+
+#by using COUNT methods
+c=["guvi","2",3]
+z=c.count(3)
+print(z)
+
+#by using EXTEND methods
+x=["guvi","zoho"]
+y=["ola","uber"]
+x.extend(y)
+print(x)
+
+#by using INDEX methods
+o=["ola","uber"]
+s=o.index("uber")
+print(s)
+
+#by using INSERT methods
+f=["guvi","zolo"]
+f.insert(1,"zoho")
+print(f)
+
+#by using POP method
+i=["cat","dog"]
+i.pop(1)
+print(i)
+
+#by using REMOVE method
+j=["cat","dog"]
+j.remove("cat")
+print(j)
+
+#by using REVERSE method
+g=["cat","dog"]
+g.reverse()
+print(g)
+
+#by using SORT method
+h=["cat","dog","amul","basic"]
+h.sort()
+print(h)
